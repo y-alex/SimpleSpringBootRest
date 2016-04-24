3f1a503f7a91de691c37acff1c5156fbba5011c3
Alex <catchyview@gmail.com>
2016-04-24 17:08:41 +0300

    Added user post impl

--- src/main/java/demo/controller/UserController.java
+++ src/main/java/demo/controller/UserController.java
@@ -3,11 +3,14 @@ package demo.controller;
 import javax.inject.Inject;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import demo.domain.User;
 import demo.repository.UserRepository;
@@ -23,5 +26,20 @@ public class UserController {
 		return new ResponseEntity<Iterable<User>>(mUserRepository.findAll(),	HttpStatus.OK);
 	}
 
+	@RequestMapping(value="/users", method=RequestMethod.POST)
+	public ResponseEntity<User> createPoll(@RequestBody User user){
+		user = mUserRepository.save(user);
+		// Set the location header for the newly created	resource
+		HttpHeaders responseHeaders = new HttpHeaders();
+		URI newUserUri = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(user.getmId())
+		.toUri();
+		responseHeaders.setLocation(newUserUri);
+		return new ResponseEntity<>(null, responseHeaders,
+		HttpStatus.CREATED);
+
+	}
 }
 
